aedd4f5cab0e84fcc5977b01b977abc3c164438b
Mind <nathapat.mind@gmail.com>
2022-12-23 16:26:59 +0700

    lab9_final3

--- lab9_3.cpp
+++ lab9_3.cpp
@@ -1,13 +1,49 @@
-/*
-"Enter your age: "
-"Enter your height: "
-"Enter your bounty: "
-"Your character = "
-"Zoro"
-"Sanji"
-"Usopp"
-"Chopper"
-"Franky"
-"Brook"
-"Jinbe"
-*/
+
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    int age, bounty;
+    string character;
+    cout << "Enter your age: ";
+    cin >> age;
+    if(age <= 25){
+        int height;
+        cout << "Enter your height: ";
+        cin >> height;
+        if(height < 100){
+            character = "Chopper";
+        }else{
+            if(height < 180){
+                character = "Usopp";
+            }else{
+                cout << "Enter your bounty: ";
+                cin >> bounty;
+                if(bounty > 11e8){
+                    character = "Zoro";
+                }else{
+                    character = "Sanji";
+                }
+            }
+        }
+
+    }else{
+        if(age <= 60){
+            cout << "Enter your bounty: ";
+            cin >> bounty;
+            if(bounty > 5e8){
+                character = "Jinbe";
+            }else{
+                character = "Franky";
+            }
+        }else{
+            character = "Brook";
+        }
+
+    }
+    cout << "Your character = " << character;
+
+    return 0;
+
+
+}
